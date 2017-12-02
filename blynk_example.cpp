0040a2265f8e4b9ba01972087e1cc6a083496e02
kadiraktass <kadir-aktas@outlook.com>
2017-12-02 16:11:33 +0200

    blynk exp

---
+++ blynk_example.cpp
@@ -0,0 +1,47 @@
+#define BLYNK_PRINT stdout
+
+#ifdef RASPBERRY
+ #include <BlynkApiWiringPi.h>
+#else
+ #include <BlynkApiLinux.h>
+#endif
+#include <BlynkSocket.h>
+#include <BlynkOptionsParser.h>
+
+static BlynkTransportSocket _blynkTransport;
+BlynkSocket Blynk(_blynkTransport);
+#include <BlynkWidgets.h>
+
+// This function will be called every time Slider Widget
+// in Blynk app writes values to the Virtual Pin V1
+BLYNK_WRITE(V1)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+
+  // process received value
+}
+
+void setup()
+{
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
+int main(int argc, char* argv[])
+{
+  const char *auth, *serv;
+  uint16_t port;
+  parse_options(argc, argv, auth, serv, port);
+
+  Blynk.begin(auth, serv, port);
+
+  setup();
+  while(true) {
+    loop();
+  }
+
+  return 0;
+}
