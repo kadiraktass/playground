0a9fdb0867ab320cf425306f441f765459ea2d9a
kadiraktass <kadir-aktas@outlook.com>
2017-12-06 02:37:43 +0200

    asd

--- main.py
+++ main.py
@@ -32,44 +32,24 @@ def my_read_handler():
     #print(pressure)
     blynk.virtual_write(5, int(pressure)) 
 
-@blynk.VIRTUAL_READ(7)
+@blynk.VIRTUAL_READ(10)
 def my_read_handler():
     rcv = read_pm_line(port) 
-    # res = {'timestamp': datetime.datetime.now(),
-       # 'apm10': rcv[4] * 256 + rcv[5],
-       # 'apm25': rcv[6] * 256 + rcv[7],
-       # 'apm100': rcv[8] * 256 + rcv[9],
-       # 'pm10': rcv[10] * 256 + rcv[11],
-       # 'pm25': rcv[12] * 256 + rcv[13],
-       # 'pm100': rcv[14] * 256 + rcv[15],
-       # 'gt03um': rcv[16] * 256 + rcv[17],
-       # 'gt05um': rcv[18] * 256 + rcv[19],
-       # 'gt10um': rcv[20] * 256 + rcv[21],
-       # 'gt25um': rcv[22] * 256 + rcv[23],
-       # 'gt50um': rcv[24] * 256 + rcv[25],
-       # 'gt100um': rcv[26] * 256 + rcv[27]
-       # }
-    rcv_unpack = struct.unpack('>16H', rcv)
-    print(type(rcv))
-    print(type(rcv_unpack))
-    print(rcv_unpack)
-    print("rcv[0] = ", rcv[0])
-    print("rcv[1] = ", rcv[1])
-    print("rcv[2] = ", rcv[2])
-    print("rcv[3] = ", rcv[3])
-    print("rcv[4] = ", rcv[4])
-    print("rcv[5] = ", rcv[5])
-    print("rcv[6] = ", rcv[6])
-    print("rcv[7] = ", rcv[7])
-    print("rcv[8] = ", rcv[8])
-    #pm10 = res['apm10']
-    #print(res)
-    #print(pm10)
-    #print('PM1.0(CF=1): {}\n'.format(pm10))
-    #pm10_f = float(pm10)
-    #print(type(pm10_f))
-    #print((pm10_f))
-    blynk.virtual_write(7, rcv[0]) 
+    blynk.virtual_write(10, rcv[4] * 256 + rcv[5]) 
+
+@blynk.VIRTUAL_READ(12)
+def my_read_handler():
+    rcv = read_pm_line(port) 
+    blynk.virtual_write(12, rcv[6] * 256 + rcv[7])
+
+@blynk.VIRTUAL_READ(14)
+def my_read_handler():
+    rcv = read_pm_line(port) 
+    blynk.virtual_write(14, rcv[8] * 256 + rcv[9]) 
+
+@blynk.VIRTUAL_READ(20)
+def my_read_handler():
+    blynk.virtual_write(20, time.time())     
     
 def read_pm_line(_port):
     #data = b''
@@ -84,39 +64,39 @@ def read_pm_line(_port):
                 return rv
 
 blynk.run() 
-while True:
-    try:
-        rcv = read_pm_line(port)
-        res = {'timestamp': datetime.datetime.now(),
-               'apm10': rcv[4] * 256 + rcv[5],
-               'apm25': rcv[6] * 256 + rcv[7],
-               'apm100': rcv[8] * 256 + rcv[9],
-               'pm10': rcv[10] * 256 + rcv[11],
-               'pm25': rcv[12] * 256 + rcv[13],
-               'pm100': rcv[14] * 256 + rcv[15],
-               'gt03um': rcv[16] * 256 + rcv[17],
-               'gt05um': rcv[18] * 256 + rcv[19],
-               'gt10um': rcv[20] * 256 + rcv[21],
-               'gt25um': rcv[22] * 256 + rcv[23],
-               'gt50um': rcv[24] * 256 + rcv[25],
-               'gt100um': rcv[26] * 256 + rcv[27]
-               }
-        print('===============\n'
-              'PM1.0(CF=1): {}\n'
-              'PM2.5(CF=1): {}\n'
-              'PM10 (CF=1): {}\n'
-              'PM1.0 (STD): {}\n'
-              'PM2.5 (STD): {}\n'
-              'PM10  (STD): {}\n'
-              '>0.3um     : {}\n'
-              '>0.5um     : {}\n'
-              '>1.0um     : {}\n'
-              '>2.5um     : {}\n'
-              '>5.0um     : {}\n'
-              '>10um      : {}'.format(res['apm10'], res['apm25'], res['apm100'],
-                                       res['pm10'], res['apm25'], res['pm100'],
-                                       res['gt03um'], res['gt05um'], res['gt10um'],
-                                       res['gt25um'], res['gt50um'], res['gt100um']))
+# while True:
+    # try:
+        # rcv = read_pm_line(port)
+        # res = {'timestamp': datetime.datetime.now(),
+               # 'apm10': rcv[4] * 256 + rcv[5],
+               # 'apm25': rcv[6] * 256 + rcv[7],
+               # 'apm100': rcv[8] * 256 + rcv[9],
+               # 'pm10': rcv[10] * 256 + rcv[11],
+               # 'pm25': rcv[12] * 256 + rcv[13],
+               # 'pm100': rcv[14] * 256 + rcv[15],
+               # 'gt03um': rcv[16] * 256 + rcv[17],
+               # 'gt05um': rcv[18] * 256 + rcv[19],
+               # 'gt10um': rcv[20] * 256 + rcv[21],
+               # 'gt25um': rcv[22] * 256 + rcv[23],
+               # 'gt50um': rcv[24] * 256 + rcv[25],
+               # 'gt100um': rcv[26] * 256 + rcv[27]
+               # }
+        # print('===============\n'
+              # 'PM1.0(CF=1): {}\n'
+              # 'PM2.5(CF=1): {}\n'
+              # 'PM10 (CF=1): {}\n'
+              # 'PM1.0 (STD): {}\n'
+              # 'PM2.5 (STD): {}\n'
+              # 'PM10  (STD): {}\n'
+              # '>0.3um     : {}\n'
+              # '>0.5um     : {}\n'
+              # '>1.0um     : {}\n'
+              # '>2.5um     : {}\n'
+              # '>5.0um     : {}\n'
+              # '>10um      : {}'.format(res['apm10'], res['apm25'], res['apm100'],
+                                       # res['pm10'], res['apm25'], res['pm100'],
+                                       # res['gt03um'], res['gt05um'], res['gt10um'],
+                                       # res['gt25um'], res['gt50um'], res['gt100um']))
 
-    except KeyboardInterrupt:
-        break
+    # except KeyboardInterrupt:
+        # break
